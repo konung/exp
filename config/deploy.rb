9727b04768050c543fd0ee8d84c83e6711b8d68b
Nick Gorbikoff <ngorbikoff@ajrinlt.com>
2017-06-30 12:52:18 -0500

    fixing migrations & capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ namespace :deploy do
   task :migrate do
     on roles(:app) do
       within release_path do
-        execute "RACK_ENV=production rake db:migrate"
+        execute "bundle exec RACK_ENV=production rake db:migrate"
       end
     end
   end
