71453abab3362c1e62fb758ead8c4fe852fe5b25
Nick Gorbikoff <ngorbikoff@ajrinlt.com>
2017-06-30 12:35:30 -0500

    fixing migrations & capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,3 +14,15 @@ set :ssh_options, { :forward_agent => true }
 set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 append :linked_files, "config/secrets.yml", ".env.production"
 append :linked_dirs, "log", "public", "tmp/pids", "tmp/cache", "tmp/sockets", "uploads"
+
+namespace :deploy do
+  desc "migrate Production db"
+  task :migrate do
+    on roles(:app) do
+      within release_path do
+        execute "rake RACK_ENV=production db:migrate"
+      end
+    end
+  end
+  after 'deploy:updated', 'deploy:migrate'
+end
