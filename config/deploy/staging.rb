6daa704f51e231940a00154beec13460db05fd86
Nick Gorbikoff <ngorbikoff@ajrinlt.com>
2017-06-30 11:34:30 -0500

    fixing migrations & capfile

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,12 @@
+#set :application, "yourapp.yourdomain.com"
+#set :deploy_to, "/data/web/your_app_folder"
+#set :user, "username"
+#set :rails_env, "test"
+#set :migrate_env, "RACK_ENV=test"
+#set :branch, "develop"
+#set :use_sudo, false
+#
+#server "127.0.0.1", :web, :app, :db, :primary => true
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
