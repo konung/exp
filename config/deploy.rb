ad1827864458449231dfa41c1607ae0daa459db7
Nick Gorbikoff <ngorbikoff@ajrinlt.com>
2017-06-30 12:05:02 -0500

    fixing migrations & capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,16 @@
-# config valid only for current version of Capistrano
 lock "3.8.2"
 
-# Instead of storing keys on the server. Make sure ssh-agent is up and running on the local machine.
+set :application, "exp"
+set :repo_url, "git@github.com:konung/exp.git"
+set :branch, "master"
+set :deploy_to, "/var/www/exp"
+set :rack_env, :production
+set :rails_env, :production
+set :user, "deploy"
+set :use_sudo, false
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
 set :ssh_options, { :forward_agent => true }
-
-# Default value for :format is :airbrussh.
-set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
 set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :linked_files is []
-append :linked_files, "config/secrets.yml", ".env.production"
-
-# Default value for linked_dirs is []
+append :linked_files, "config/secrets.yml", ".env.production", "tmp/restart.txt"
 append :linked_dirs, "log", "public", "tmp/pids", "tmp/cache", "tmp/sockets", "uploads"
-
-# Default value for keep_releases is 5
-set :keep_releases, 5
-
-set :passenger_restart_with_touch, true
-
-set :migration_role, :app
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,4 @@
-server "isascam.com", :user => "deploy", :roles => %{web app db}
-set :application, "exp.trb.to"
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/exp"
-
-set :scm, :git
-set :repo_url, "git@github.com:konung/exp.git"
-set :branch, "master"
-
-set :rack_env, :production
-set :rails_env, :production
-set :user, "deploy"
-set :use_sudo, false
+server 'isascam.com', user: 'deploy', roles: %w{app web}
+set :bundle_jobs, 1
+set :migration_role, :app
+set :passenger_restart_with_touch, false
